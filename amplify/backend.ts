3e8e6417d4c1ad31e8447f50403b72c58408b8f7
Workshop Participant <participant@example.com>
2026-02-05 08:40:16 +0000

    Added forms

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,18 +1,17 @@
+import * as cognito from 'aws-cdk-lib/aws-cognito';
 import { defineBackend } from '@aws-amplify/backend'; // Используем defineBackend вместо класса Backend
 import { auth } from './auth/resource';
 import { data } from './data/resource';
-import { LocationMapStack } from './custom/maps/resource';
 
-/**
- * @see https://docs.amplify.aws/react/build-system/deploy-anywhere/backend-definition/
- */
 const backend = defineBackend({
   auth,
   data,
 });
 
-new LocationMapStack(
-  backend.createStack('LocationMapStack'), // В новых версиях используется createStack
-  'myLocationResource',
-  {}
-);
+const userPoolId = backend.auth.resources.userPool.userPoolId;
+const authGroupStack = backend.createStack('AuthGroupStack');
+const adminGroup = new cognito.CfnUserPoolGroup(authGroupStack, 'AdminGroup', {
+  userPoolId,
+  groupName: 'Admin',
+  description: 'Admin group',
+});
---
+++ ui-components/index.js
@@ -0,0 +1,4 @@
+export { default as ProductCreateForm } from "./ProductCreateForm";
+export { default as ProductUpdateForm } from "./ProductUpdateForm";
+export { default as CategoryCreateForm } from "./CategoryCreateForm";
+export { default as CategoryUpdateForm } from "./CategoryUpdateForm";
